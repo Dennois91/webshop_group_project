e33a0ca7788d57b752f61d611d36258a26d28156
Lukas Chyle <lukas_chyle@hotmail.com>
2023-03-20 02:17:29 +0100

    Fetch and display products from fakestoreapi

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('https://fakestoreapi.com/products')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendImages(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+function appendImages(data) {
+    const mainContainer = document.getElementById("getProductImages");
+    for (let i = 0; i < data.length; i++) {
+        const element = document.createElement("div");
+        element.classList.add("col-sm");
+        element.innerHTML = "" + data[i].title + '<br>' + '<img src=' + data[i].image + ' alt="product-picture">'
+        mainContainer.appendChild(element);
+    }
+}
+
