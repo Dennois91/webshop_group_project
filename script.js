73b549b8d6103e74956e8b9113de83df89ce939e
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-03-21 15:44:21 +0100

    La en komment i JS.

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ function appendImages(data) {
     }
 }
 
+// Uppdaterad version som gör den responsiv och centrerar bilderna.
+//
+// function appendImages(data) {
+//     const mainContainer = document.getElementById("getProductImages");
+//     for (let i = 0; i < data.length; i++) {
+//         const element = document.createElement("div");
+//         element.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-12");
+//
+//         const contentWrapper = document.createElement("div");
+//         contentWrapper.classList.add("d-flex", "justify-content-center", "flex-column", "align-items-center");
+//
+//         contentWrapper.innerHTML =
+//             '<p class="price">' + data[i].price + " €" + '</p>' +
+//             '<p>' + data[i].title + '</p>' +
+//             '<img src=' + data[i].image + ' class="img-fluid" alt="product-picture">';
+//
+//         element.appendChild(contentWrapper);
+//         mainContainer.appendChild(element);
+//     }
+// }
+
