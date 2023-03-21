71a7b9ed153aca366c14001f8cf39f1d2aa1fe0f
Lukas Chyle <lukas_chyle@hotmail.com>
2023-03-21 17:50:48 +0100

    JS-changes

--- script.js
+++ script.js
@@ -21,33 +21,14 @@ function appendImages(data) {
         const element = document.createElement("div");
         element.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-12");
         element.innerHTML =
+            '<div class="product-box">' +
             '<p class="price">' + data[i].price + " €" + '</p>' + '<br>' +
             '<p>' + data[i].title + '</p>' + '<br>' +
-            '<img src=' + data[i].image + ' class="img-fluid" alt="product-picture">';
+            '<img src=' + data[i].image + ' class="img-fluid" alt="product-picture">' +
+            '</div>';
 
         mainContainer.appendChild(element);
     }
 }
-
-// Uppdaterad version som gör den responsiv och centrerar bilderna.
-
-// function appendImages(data) {
-//     const mainContainer = document.getElementById("getProductImages");
-//     for (let i = 0; i < data.length; i++) {
-//         const element = document.createElement("div");
-//         element.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-12");
-//
-//         const contentWrapper = document.createElement("div");
-//         contentWrapper.classList.add("d-flex", "justify-content-center", "flex-column", "align-items-center");
-//
-//         contentWrapper.innerHTML =
-//             '<p class="price">' + data[i].price + " €" + '</p>' +
-//             '<p>' + data[i].title + '</p>' +
-//             '<img src=' + data[i].image + ' class="img-fluid" alt="product-picture">';
-//
-//         element.appendChild(contentWrapper);
-//         mainContainer.appendChild(element);
-//     }
-// }
 
 // TODO: MAX fixar formulär-sidan och bekräftelse-sidan. (Utcheckning och bekräftelse)
