174e0495e833de6c53b39ff9b558cd0b0defd24a
Lukas Chyle <lukas_chyle@hotmail.com>
2023-03-21 17:35:24 +0100

    JS-changes

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-fetch('https://fakestoreapi.com/products')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        appendImages(data);
-    })
-    .catch(function (err) {
-        console.log('error: ' + err);
-    });
+
+
+
+
+//function fetchAllProducts() {
+    fetch('https://fakestoreapi.com/products') // TODO: Sätt in i en funktion, så att alla pages inte behöver ladda ner produkterna.
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            appendImages(data);
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+//}
 
 function appendImages(data) {
     const mainContainer = document.getElementById("getProductImages");
     for (let i = 0; i < data.length; i++) {
         const element = document.createElement("div");
-        element.classList.add("col-sm-2");
+        element.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-12");
         element.innerHTML =
             '<p class="price">' + data[i].price + " €" + '</p>' + '<br>' +
             '<p>' + data[i].title + '</p>' + '<br>' +
             '<img src=' + data[i].image + ' class="img-fluid" alt="product-picture">';
+
         mainContainer.appendChild(element);
     }
 }
 
 // Uppdaterad version som gör den responsiv och centrerar bilderna.
-//
+
 // function appendImages(data) {
 //     const mainContainer = document.getElementById("getProductImages");
 //     for (let i = 0; i < data.length; i++) {
@@ -43,3 +50,4 @@ function appendImages(data) {
 //     }
 // }
 
+// TODO: MAX fixar formulär-sidan och bekräftelse-sidan. (Utcheckning och bekräftelse)
