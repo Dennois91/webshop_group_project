07b914050bcde0bf08a87c5158f7742403610bbf
Lukas Chyle <lukas_chyle@hotmail.com>
2023-03-22 09:03:02 +0100

    wrapped databas-fetches into functions.

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-
-
-
-
-//function fetchAllProducts() {
+function fetchAllProducts() {
     fetch('https://fakestoreapi.com/products')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            appendImages(data);
+            appendPreviews(data);
         })
         .catch(function (err) {
             console.log('error: ' + err);
         });
-//}
+}
 
-function appendImages(data) {
-    const mainContainer = document.getElementById("getProductImages");
+function appendPreviews(data) {
+    const mainContainer = document.getElementById("getProductPreviews");
     for (let i = 0; i < data.length; i++) {
         const element = document.createElement("div");
         element.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-12");
@@ -28,6 +24,34 @@ function appendImages(data) {
             '</div>';
         mainContainer.appendChild(element);
     }
+}
+
+function fetchProduct(id) {
+    fetch('https://fakestoreapi.com/products/' + id)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            appendProduct(data);
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+}
+
+function appendProduct(data) {
+    const mainContainer = document.getElementById("getProduct");
+    const element = document.createElement("div");
+    element.classList.add("product");
+    element.innerHTML =
+        '<div class="product-box">' +
+        '<p class="price">' + data.price + " €" + '</p>' + '<br>' +
+        '<p class="category">' + data.category + " €" + '</p>' + '<br>' +
+        '<p class="title">' + data.title + '</p>' + '<br>' +
+        '<img src=' + data.image + ' class="img-fluid" alt="product-picture">' + '<br>' +
+        '<p class="description">' + data.description + '</p>' +
+        '</div>';
+    mainContainer.appendChild(element);
 }
 
 // TODO: MAX fixar formulär-sidan och bekräftelse-sidan. (Utcheckning och bekräftelse)
